28b54a88a9e7bb608a20f34bdca43ae0841cac42
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-04 09:16:19 -0300

    Desafio 3 realizado

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  return frase.split(' ');
 }
 
 // Desafio 4
