5b922a5242e9ce487b07214fe5ee2d7c9463d52d
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-04 23:26:10 -0300

    correçao desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -56,10 +56,10 @@ function catAndMouse(mouse, cat1, cat2) {
   let dist1 = Math.abs(cat1 - mouse);
   let dist2 = Math.abs(cat2 - mouse);
   if (dist1 < dist2) {
-    return cat1;
+    return 'cat1';
   }
   if (dist2 > dist1) {
-    return cat2;
+    return 'cat2';
   }
   if (dist1 === dist2) {
     return ('Os gatos trombam e o rato sai foge');
