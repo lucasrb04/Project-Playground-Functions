02c3fc7d4b14aff1f1f128eac5e0b8ef9fe979fb
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-20 17:59:27 -0300

    adicionando requisito 10 techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,17 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, string) {
+  const orderedArray = array.sort();
+  let objectArray = [];
+  for (let index = 0; index < array.length; index += 1) {
+    let myObject = {
+      tech: orderedArray[index],
+      name: string,
+    };
+    objectArray.push(myObject);
+  }
+  return objectArray;
 }
-
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
